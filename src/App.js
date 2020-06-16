07a39d08b4854770a7603f75ffe9ad92d3851be3
Dani DeSalvio <danidvisuals@gmail.com>
2020-06-15 18:05:12 -0600

    add bones

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Nav from './components/shared/Nav';
+import Home from './components/pages/Home';
+import Work from './components/pages/Work';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import NoMatch from './components/shared/NoMatch';
 
-
-class App extends React.Component {
-  render () {
-    return(
-      <>
-      <Nav/>
-      </>
-    )
-  }
-}
+const App = () => (
+  <>
+    <Nav/>
+    <Fragment>
+      <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/work" component={Work} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={NoMatch}/>
+      </Switch>
+    </Fragment>
+  </>
+);
 
 export default App;
